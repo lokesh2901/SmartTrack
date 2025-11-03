84692d3b27bfc442f0f3e752df843d4a2e1a02b5
Lokesh <lokeshaadarsh@gmail.com>
2025-11-04 02:26:15 +0530

    final push

--- smarttrack-frontend/smarttrack-frontend/src/services/authService.js
+++
@@ -1,8 +0,0 @@
-import api from "./api";
-
-export const login = (payload) => api.post("/users/login", payload);
-export const forgotPassword = (email) => api.post("/users/forgot-password", { email });
-export const verifyOtp = (payload) => api.post("/users/verify-otp", payload);
-export const resetPassword = (payload) => api.post("/users/reset-password", payload);
-
-export default { login, forgotPassword, verifyOtp, resetPassword };
--- smarttrack-frontend/smarttrack-frontend/src/services/userService.js
+++
@@ -1,8 +0,0 @@
-import api from './api'
-
-
-const getUsers = () => api.get('/users')
-const getMe = () => api.get('/users/me')
-
-
-export default { getUsers, getMe }
