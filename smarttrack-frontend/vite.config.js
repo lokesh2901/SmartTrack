f878d6179945dc35637ee4c2b4a99dac50db3c73
Lokesh <lokeshaadarsh@gmail.com>
2025-11-04 04:36:55 +0530

    verce.json updtaed

--- smarttrack-frontend/vite.config.js
+++ smarttrack-frontend/vite.config.js
@@ -1,13 +1,7 @@
-{
-  "builds": [
-    {
-      "src": "package.json",
-      "use": "@vercel/static-build",
-      "config": { "distDir": "dist" }
-    }
-  ],
-  "routes": [
-    { "handle": "filesystem" },
-    { "src": "/(.*)", "dest": "/" }
-  ]
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
